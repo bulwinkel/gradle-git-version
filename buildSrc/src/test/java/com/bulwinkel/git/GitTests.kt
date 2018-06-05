a12b4a4ccbf01f4d8a88ccc44114ac4c4e46285d
Bulwinkel <kelvin@bulwinkel.com>
2018-06-06 08:57:06 +1000

    Update tests

--- buildSrc/src/test/java/com/bulwinkel/git/GitTests.kt
+++ buildSrc/src/test/java/com/bulwinkel/git/GitTests.kt
@@ -1,16 +1,25 @@
 package com.bulwinkel.git
 
+import io.kotlintest.matchers.beGreaterThan
+import io.kotlintest.matchers.contain
+import io.kotlintest.matchers.should
 import io.kotlintest.matchers.shouldBe
+import io.kotlintest.matchers.shouldEqual
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
 
 class GitTests : Spek({
+
+    val initialTag = "0.0.0"
+
     describe("Tag") {
         on("list") {
             it("should return a list of all tags") {
-                Git.tag.list().isEmpty() shouldBe false
+                val tags = Git.tag.list()
+                tags.isEmpty() shouldBe false
+                tags should contain(initialTag)
             }
         }
     }
@@ -18,10 +27,14 @@ class GitTests : Spek({
     describe("Describe") {
         on("match") {
             it("should describe a tag given as the pattern") {
-                val tag = "0.0.0"
-                val description = Git.describe.match(tag)
-                println("tag = $tag, description = $description")
+                val description = Git.describe.match(initialTag)
+                val (version, commitCount, shortHash) = description.split("-")
+                println("version = $version, commitCount = $commitCount, shortHash = $shortHash")
+
                 description.isNotBlank() shouldBe true
+                version shouldEqual initialTag
+                commitCount.toInt() should beGreaterThan(0)
+                shortHash shouldEqual "gf9ee8b22cbc7"
             }
         }
     }
