a17c163fe23b59932ef66d0283b0d54dc62e3ae6
Bulwinkel <kelvin@bulwinkel.com>
2018-06-18 16:10:28 +1000

    Add task for displaying the current git version in the console

--- buildSrc/src/main/java/com/bulwinkel/gitversion/GitVersionPlugin.kt
+++ buildSrc/src/main/java/com/bulwinkel/gitversion/GitVersionPlugin.kt
@@ -2,9 +2,28 @@ package com.bulwinkel.gitversion
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.TaskContainer
 
 open class GitVersionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
-        target.extensions.extraProperties["gitVersion"] = GitVersion()
+        val gitVersion = GitVersion()
+        target.extensions.extraProperties["gitVersion"] = gitVersion
+        target.tasks.createVersionReportTask(gitVersion)
+    }
+
+    private fun TaskContainer.createVersionReportTask(gitVersion: GitVersion) {
+        create("versionReport") {
+            group = "versioning"
+            doLast {
+                println("""
+
+                    GitVersion Report
+                    -----------------
+                    gitVersion.name = ${gitVersion.name}
+                    gitVersion.buildNumber = ${gitVersion.buildNumber}
+
+                """.trimIndent())
+            }
+        }
     }
 }
